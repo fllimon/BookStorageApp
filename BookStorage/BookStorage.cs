6860206abb05b774cd5144b74a980c3ae6a4df2e
Artur Sharaenko <49069796+fllimon@users.noreply.github.com>
2021-10-06 12:33:02 +0300

    add BookStorage

---
+++ BookStorage/BookStorage.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookStorage
+{
+    class BookStorage : IBookStorage
+    {
+        public bool AddBook(Book someBook)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool DeleteBook(Book someBook)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Book> GetAllBooks()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool UpdateBook(Book someBook)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ BookStorage/IBookStorage.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookStorage
+{
+    interface IBookStorage
+    {
+        public bool AddBook(Book someBook);
+
+        public IEnumerable<Book> GetAllBooks();
+
+        public bool UpdateBook(Book someBook);
+
+        public bool DeleteBook(Book someBook);
+    }
+}
