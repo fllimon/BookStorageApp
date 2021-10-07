e99aaed467b14d5b8f3a0c839f136de503e555c0
Artur Sharaenko <49069796+fllimon@users.noreply.github.com>
2021-10-07 10:03:34 +0300

    Write AddBook method

--- BookStorage/BookStorage.cs
+++ BookStorage/BookStorage.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 
 namespace BookStorage
@@ -8,7 +9,18 @@ namespace BookStorage
     {
         public bool AddBook(Book someBook)
         {
-            throw new NotImplementedException();
+            bool isAddBook = false;
+
+            if (someBook == null)
+            {
+                return isAddBook;
+            }
+
+            FileManager fManager = new FileManager();
+
+            fManager.WriteRecordInFile(someBook);
+
+            return isAddBook = true;
         }
 
         public bool DeleteBook(Book someBook)
