e41dd17fd56e0120a7a177ebfce1fe25e205f6b8
Artur Sharaenko <49069796+fllimon@users.noreply.github.com>
2021-10-07 11:01:15 +0300

    add get books method

--- BookStorage/FileManager.cs
+++ BookStorage/FileManager.cs
@@ -8,6 +8,8 @@ namespace BookStorage
 {
     class FileManager
     {
+        private readonly JsonSerializer _serializer = new JsonSerializer();
+
         public void WriteRecordInFile(Book book)
         {
             using (StreamWriter writer = new StreamWriter(DeffaultSettings.TXT_FILE, true))
@@ -15,6 +17,24 @@ namespace BookStorage
                 var data = JsonConvert.SerializeObject(book, Formatting.Indented);
 
                 writer.WriteLine(data);
+            }
+        }
+
+        public IEnumerable<Book> GetBooksFromFile()
+        {
+            IList<Book> lists = new List<Book>();
+
+            using (var reader = new StreamReader(DeffaultSettings.TXT_FILE))
+            {
+                using (var json = new JsonTextReader(reader))
+                {
+                    if (json == null)
+                    {
+                        throw new NullReferenceException();
+                    }
+
+                    yield return _serializer.Deserialize<Book>(json);
+                }
             }
         }
     }
