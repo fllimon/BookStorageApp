d8edbc827d9bf47b44c0881fd1cc528e53f009f8
Artur Sharaenko <49069796+fllimon@users.noreply.github.com>
2021-10-07 10:04:18 +0300

    Add DefaultSettings class

---
+++ BookStorage/DeffaultSettings.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookStorage
+{
+    class DeffaultSettings
+    {
+        public const string TXT_FILE = @".\Book.json";
+    }
+}
