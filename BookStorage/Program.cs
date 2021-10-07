8dd84d1c5181523c048210e7cc0201c2b857f9a5
Artur Sharaenko <49069796+fllimon@users.noreply.github.com>
2021-10-07 12:12:18 +0300

    Modify

--- BookStorage/Program.cs
+++ BookStorage/Program.cs
@@ -8,12 +8,17 @@ namespace BookStorage
         {
             Book book = new Book("Math", "school book", "Unknown Author", 700, "12.09.2008");
             Book book2 = new Book("History", "school book", "В.В Васильев", 500, "16.10.2000");
+            Book book3 = new Book("History", "school book", "A.A Васильев", 700, "16.10.2010");
+
             BookStorage storage = new BookStorage();
 
             storage.AddBook(book);
             storage.AddBook(book2);
 
-            var data = storage.GetBooks();
+            //bool result = storage.DeleteBook(book3);
+
+            bool result2 = storage.UpdateBookByTitle(book2, book3);
+            //var data = storage.GetBooks();
 
             Console.ReadKey();
         }
