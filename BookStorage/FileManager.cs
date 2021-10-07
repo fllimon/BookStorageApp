b1abe63f510128e055903573fe13bc7621d95884
Artur Sharaenko <49069796+fllimon@users.noreply.github.com>
2021-10-07 10:03:53 +0300

    Add FileManager

---
+++ BookStorage/FileManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace BookStorage
+{
+    class FileManager
+    {
+        public void WriteRecordInFile(Book book)
+        {
+            using (StreamWriter writer = new StreamWriter(DeffaultSettings.TXT_FILE, true))
+            {
+                var data = JsonConvert.SerializeObject(book, Formatting.Indented);
+
+                writer.WriteLine(data);
+            }
+        }
+    }
+}
